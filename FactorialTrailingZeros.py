09999f23762db7234735404d87de52a2d06072a4
Saurabh Patil <saurabh.s.patil8989@gmail.com>
2016-11-11 00:00:47 -0600

    Find trailing zeros for factorials

--- FactorialTrailingZeros.py
+++ FactorialTrailingZeros.py
@@ -1,3 +1,7 @@
+# Given an integer n, return the number of trailing zeroes in n!.
+
+# Note: Your solution should be in logarithmic time complexity.
+
 class Solution(object):
     def trailingZeroes(self, n):
         """
